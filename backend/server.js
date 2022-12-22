a3c40041ec4aa34e86b51163b48b065ae6b547f9
Minesh Tandel <minesh9174@gmail.com>
2022-12-22 17:23:05 -0500

    Environment Chnaged

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const errorHandler = require('./middlewares/donationMiddleware')
 const connectDB = require('./config/db')
@@ -13,5 +14,13 @@ app.use(express.urlencoded({extended: false}))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/donations', require('./routes/donationRoutes'))
 
+//Frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'incex.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please Activate Production'))
+}
 app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
