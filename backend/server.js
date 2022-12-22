60885016cd5aec4c9559f7e10fe640c85a32f6db
Minesh Tandel <minesh9174@gmail.com>
2022-12-22 18:15:12 -0500

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/donations', require('./routes/donationRoutes'))
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'incex.html')))
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please Activate Production'))
 }
